808b8a62ca8d421d430b3aef58cce828ed613448
yuanzk <zm212896@163.com>
2023-09-27 11:43:46 +0800

    Create IM.java

---
+++ lambda-function/src/main/java/com/examle/entity/IM.java
@@ -0,0 +1,14 @@
+package com.example.entity;
+
+import lombok.Data;
+
+/**
+ * IM
+ *
+ * @author xxx
+ * @since 2023/9/20 16:55
+ */
+@Data
+public class IM extends Communication {
+    private String code;
+}
