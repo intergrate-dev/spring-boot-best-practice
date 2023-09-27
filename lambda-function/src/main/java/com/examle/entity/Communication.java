5a7466e051685e035f8048fbc71e6b5bec02e7bc
yuanzk <zm212896@163.com>
2023-09-27 11:42:13 +0800

    Create Communication.java

---
+++ lambda-function/src/main/java/com/examle/entity/Communication.java
@@ -0,0 +1,14 @@
+package com.example.entity;
+
+import lombok.Data;
+
+/**
+ * Communication
+ *
+ * @author xxx
+ * @since 2023/9/20 16:54
+ */
+@Data
+public class Communication {
+    private int type;
+}
